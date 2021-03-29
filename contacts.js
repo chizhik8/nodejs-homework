98536738e1db5679a768ae60d1275926f7772f5c
julia_chizhik <for.yu.ch@gmail.com>
2021-03-29 18:09:25 +0300

    summary

--- contacts.js
+++ contacts.js
@@ -2,22 +2,16 @@ const path = require('path');
 const fs = require('fs').promises;
 
 const contactsPath = path.join(__dirname, 'db', 'contacts.json');
-// console.log('contactsPath:', contactsPath);
-
-// В функциях используй модуль fs и его методы readFile() и writeFile()
-// TODO: задокументировать каждую функцию
 
 function listContacts() {
-  console.log('Вызов listContacts()');
   fs.readFile(contactsPath)
     .then(data => {
-      console.log(data.toString());
+      console.table(JSON.parse(data));
     })
     .catch(err => console.error(err.message));
 }
 
 function getContactById(contactId) {
-  console.log('Вызов getContactById(contactId)');
   fs.readFile(contactsPath)
     .then(data => JSON.parse(data))
     .then(contacts => {
@@ -31,7 +25,6 @@ function getContactById(contactId) {
 }
 
 function removeContact(contactId) {
-  console.log('Вызов removeContact(contactId)');
   fs.readFile(contactsPath)
     .then(data => JSON.parse(data))
     .then(contacts =>
@@ -41,7 +34,6 @@ function removeContact(contactId) {
 }
 
 function addContact(name, email, phone) {
-  console.log('Вызов addContact(name, email, phone)');
   fs.writeFile(contactsPath, name, { flag: 'a+' }).catch(err =>
     console.error(err),
   );
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const argv = require('yargs').argv;
+
 const {
   listContacts,
   getContactById,
@@ -5,7 +7,28 @@ const {
   addContact,
 } = require('./contacts.js');
 
-// listContacts();
-// getContactById(4);
-// removeContact(6);
-// addContact('name', 'email', 'phone');
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      listContacts();
+      break;
+
+    case 'get':
+      getContactById(id);
+      break;
+
+    case 'add':
+      addContact(name, email, phone);
+      break;
+
+    case 'remove':
+      removeContact(id);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
